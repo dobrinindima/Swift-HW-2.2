1dfeadec139c982b2533a9130ef2c991ed8c3a53
Dmitry <dmytro.dobrynin@rubygarage.org>
2022-07-28 19:48:15 +0300

    Add HW

--- Block HW/ViewController.swift
+++ Block HW/ViewController.swift
@@ -9,11 +9,57 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var viewColor: UIView!
+    // MARK: - Lables
+    @IBOutlet weak var redLable: UILabel!
+    @IBOutlet weak var greenLable: UILabel!
+    @IBOutlet weak var blueLable: UILabel!
+    // MARK: - Sliders
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        [redSlider, greenSlider, blueSlider].forEach{
+            $0?.value = 1
+            $0?.minimumValue = 1
+            $0?.maximumValue = 255
+        }
+
+        redSlider.minimumTrackTintColor = .red
+        redSlider.thumbTintColor = .red
+    
+        greenSlider.minimumTrackTintColor = .green
+        greenSlider.thumbTintColor = .green
+    
+        blueSlider.minimumTrackTintColor = .blue
+        blueSlider.thumbTintColor = .blue
+    
+        redLable.text = String(Int(redSlider.value))
+        greenLable.text = String(Int(greenSlider.value))
+        blueLable.text = String(Int(blueSlider.value))
+        }
+    
+    // MARK: - Actions
+    @IBAction func redSliderChange(_ sender: Any) {
+        redLable.text = String(Int(redSlider.value))
+        changeColor()
     }
-
-
+    @IBAction func greenSliderChange(_ sender: Any) {
+        greenLable.text = String(Int(greenSlider.value))
+        changeColor()
+    }
+    @IBAction func blueSliderChange(_ sender: Any) {
+        blueLable.text = String(Int(blueSlider.value))
+        changeColor()
+    }
+    
+    private func changeColor() {
+        viewColor.backgroundColor = UIColor(red: CGFloat(redSlider.value/255), green: CGFloat(greenSlider.value/255), blue: CGFloat(blueSlider.value/255), alpha: 1)
+    }
 }
+
+
 
